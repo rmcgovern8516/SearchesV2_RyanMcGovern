0c45627d8d62e6676b9ee7283342db499c8d6415
BT_1N3_32 <rmcgovern8516@bths.edu>
2019-01-17 08:36:07 -0500

    Version 2

--- src/Runner.java
+++ src/Runner.java
@@ -4,15 +4,19 @@ public class Runner {
     {
         int[] arr1 = {-20,3,15,81,432};
 
+        // test when the target is in the middle
         int index1 = SearchUtil.binarySearch(arr1,15);
         System.out.println(index1);
 
+        // test when the target is the frst item in the array
         int index2 = SearchUtil.binarySearch(arr1,-20);
         System.out.println(index2);
 
+        // test when the target is in the array - last
         int index3 = SearchUtil.binarySearch(arr1,432);
         System.out.println(index3);
 
+        // test when the target is not in the array
         int index4 = SearchUtil.binarySearch(arr1,53);
         System.out.println(index4);
     }
